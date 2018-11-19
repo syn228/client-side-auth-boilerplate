51c6a0491e3bc8e9851712e1fe26045d86571d0d
Aaron Na <syn228@nyu.edu>
2018-11-18 18:59:45 -0800

    utilized thunk and fixed cors error to make request to the backend api

--- src/components/auth/Signup.js
+++ src/components/auth/Signup.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
-import { reduxForm, Field } from 'redux-form'
+import { reduxForm, Field } from 'redux-form';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import * as actions from '../../actions';
 
 class Signup extends Component {
+  onSubmit = (formProps) => {
+    this.props.signup(formProps);
+  };
+
   render() {
+    const { handleSubmit } = this.props
     return (
-      <form>
+      <form onSubmit={handleSubmit(this.onSubmit)}>
         <fieldset>
           <label>Email</label>
           <Field
@@ -23,9 +31,13 @@ class Signup extends Component {
             autoComplete="none"
           />
         </fieldset> 
+        <button>Sign Up</button>
       </form>
     );
   }
 }
 
-export default reduxForm({ form: 'signup' })(Signup);
+export default compose (
+  connect(null, actions),
+  reduxForm({ form: 'signup' })
+)(Signup);
