f6c3c52c705b91cedc8c3e61a9830c40a9f61ad9
Aaron Na <syn228@nyu.edu>
2018-11-18 20:39:39 -0800

    added functionality to display error when the API request fails

--- src/components/auth/Signup.js
+++ src/components/auth/Signup.js
@@ -10,6 +10,8 @@ class Signup extends Component {
   };
 
   render() {
+    console.log("test", this.props.errorMessage);
+    
     const { handleSubmit } = this.props
     return (
       <form onSubmit={handleSubmit(this.onSubmit)}>
@@ -31,13 +33,22 @@ class Signup extends Component {
             autoComplete="none"
           />
         </fieldset> 
+        <div>
+          {this.props.errorMessage}
+        </div>
         <button>Sign Up</button>
       </form>
     );
   }
 }
 
+function mapStateToProps(state) {
+  return {
+    errorMessage: state.auth.errorMessage
+  };
+};
+
 export default compose (
-  connect(null, actions),
+  connect(mapStateToProps, actions),
   reduxForm({ form: 'signup' })
 )(Signup);
