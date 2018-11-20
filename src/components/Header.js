d251fc9636804db1ffcd154d59a70e4a65978d62
Aaron Na <syn228@nyu.edu>
2018-11-19 19:42:38 -0800

    added some css and conditional render to the headers based on authentication status

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,41 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
+import './HeaderStyle.css'
 
 class Header extends Component {
+  renderLinks() {
+    if (this.props.authenticated) {
+      return (
+        <div>
+          <Link to="/signout">Sign Out</Link>
+          <Link to="/feature">Feature</Link>  
+        </div>
+      )
+    }
+    else {
+      return (
+        <div>
+          <Link to="/signup">Sign Up</Link>
+          <Link to="/signin">Sign In</Link>
+        </div>
+      )
+    }
+  };
+
   render() {
     return (
-      <div>
-        <Link to="/">Redux Auth</Link>
-        <Link to="/signup">Sign Up</Link>
-        <Link to="/signin">Sign In</Link>
-        <Link to="/signout">Sign Out</Link>
-        <Link to="/feature">Feature</Link>
+      <div className="header">
+        <Link to="/">Home</Link>
+        {this.renderLinks()}
       </div>
     );
   }
 }
+function mapStateToProps(state){
+  return {
+    authenticated: state.auth.authenticated
+  }
+}
 
-export default Header
+export default connect(mapStateToProps)(Header)
